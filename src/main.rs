8a1d52053bc7b85d15ec19c936b8f26d1e6f550a
Prashant <prashantco111@gmail.com>
2025-02-28 18:15:26 +0530

    Removed unnecessary code

--- src/main.rs
+++ src/main.rs
@@ -15,39 +15,15 @@ struct Args {
 
 #[derive(Debug, Subcommand)]
 enum Command {
-    Init {
-        name: String,
-    },
-    CreateTheme {
-        name: String,
-    },
-    SetTheme {
-        name: String,
-    },
-    Build,
-    Test,
-    Dev {
-        path: String,
-        #[command(subcommand)]
-        command: Option<DevCommand>,
-    },
-}
-
-#[derive(Debug, Subcommand)]
-enum DevCommand {
+    Init { name: String },
     CreateTheme { name: String },
+    SetTheme { name: String },
     Build,
     Test,
-    SetTheme { name: String },
 }
 
 fn main() -> Result<()> {
     let args = Args::parse();
-
-    // If it's a Dev command, change directory before processing
-    if let Command::Dev { path, .. } = &args.command {
-        std::env::set_current_dir(path)?;
-    }
 
     match args.command {
         Command::Init { name } => init::invoke(&name),
@@ -59,21 +35,5 @@ fn main() -> Result<()> {
             println!("Config: {:?}", config);
             Ok(())
         }
-        Command::Dev { path: _, command } => match command {
-            Some(DevCommand::CreateTheme { name }) => theme_handler::create_theme(&name),
-            Some(DevCommand::Build) => build::invoke(),
-            Some(DevCommand::SetTheme { name }) => theme_handler::set_theme(&name),
-            Some(DevCommand::Test) => {
-                let config = config_handler::read_config()?;
-                println!("Config: {:?}", config);
-                Ok(())
-            }
-            None => {
-                let config = config_handler::read_config()?;
-                println!("Running in development mode");
-                println!("Config: {:?}", config);
-                Ok(())
-            }
-        },
     }
 }
